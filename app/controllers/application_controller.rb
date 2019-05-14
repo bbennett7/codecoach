b556d49f49b790389dadace198cfef0666210de3
bbennett7 <bryn.bennett@gmail.com>
2019-05-13 22:53:42 -0700

    Add student and mentor login forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+  def mentor_login
+    @mentor = Mentor.new
+  end
+
+  def student_login
+    @student = Student.new 
+  end
+
   def logout
     reset_session
     redirect_to root_path
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'application#index'
 
+  get '/mentor_login', to: 'application#mentor_login'
+
+  get '/student_login', to: 'application#student_login'
+
   get '/logout', to: 'application#logout'
 
 end
