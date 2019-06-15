03e3ef2a4d8d8dc442d449d49563ab5e781dbcde
bbennett7 <bryn.bennett@gmail.com>
2019-06-15 10:53:52 -0700

    Move profile javascript back to html file to avoid flicker

--- app/assets/javascripts/languages.js
+++ app/assets/javascripts/languages.js
@@ -2,21 +2,18 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-$(document).ready(function(){
 
-
-  $('#home').click(function(event) {
-    alert("sup")
-    let currentUrl = document.location.href
-
-    var languagesDiv = document.getElementById("languages");
-
-    fetch(currentUrl)
-      .then( response => response.json())
-      .then( response => languagesDiv.innerHTML = listOfLanguages(response.languages) )
-
-    function listOfLanguages(languages) {
-      const languagesList = languages.map(language => `<li>${language.name}</li>`)
-      return `<ul>${languagesList.join("")}</ul>`}
-  });
-});
+// $(document).ready(function(){
+//   var languagesDiv = document.getElementById("languages");
+//
+//   var currentUserUrl = "http://localhost:3000" + languagesDiv.dataset.userUrl;
+//
+//   fetch(currentUserUrl)
+//     .then( response => response.json())
+//     .then( response => languagesDiv.innerHTML = listOfLanguages(response.languages) )
+//
+//   function listOfLanguages(languages) {
+//     const languagesList = languages.map(language => `<li>${language.name}</li>`)
+//     return `<ul>${languagesList.join("")}</ul>`
+//   }
+// });
