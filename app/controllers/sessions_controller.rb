c5573694fdb258f7697b4e28978ee49305d0eb05
bbennett7 <bryn.bennett@gmail.com>
2019-07-03 18:19:32 -0700

    Add new and create session methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,20 @@
 class SessionsController < ApplicationController
   # before_action :current_user
   # before_action :logged_out?, only: [:new]
-  #
-  # def new
-  #   @user = User.new
-  # end
-  #
-  # def create
-  #   @user = User.find_by(username: params[:username])
-  #   if @user && @user.authenticate(params[:password])
-  #     session[:user_id] = @user.id
-  #     redirect_to user_path(@user)
-  #   else
-  #     redirect_to login_path
-  #   end
-  # end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to login_path
+    end
+  end
   #
   # def mentor_gh_create
   #   @user = User.find_or_create_by(uid: auth['uid']) do |u|
@@ -36,9 +36,9 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
-  # private
-  #
-  # def auth
-  #  request.env['omniauth.auth']
-  # end
+  private
+
+  def auth
+   request.env['omniauth.auth']
+  end
 end
