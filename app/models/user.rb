1bd579fe19927a0ecf4e53145a5d22ce4c677a9b
bbennett7 <bryn.bennett@gmail.com>
2019-07-05 19:31:46 -0700

    Fix styling of error on user type signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,11 +14,12 @@ class User < ApplicationRecord
   validates :username, presence: { message: 'cannot be blank.' }
   validates :username, uniqueness: { message: 'is already taken.' }
   validates :username, length: { minimum: 8, message: 'must be at least 8 characters.' }
+  validates :user_type, presence: { message: 'cannot be blank.' }
   validates :first_name, presence: { message: 'cannot be blank.' }
   validates :email, presence: { message: 'cannot be blank.' }
   validates :email, uniqueness: { message: 'is associated with an existing account.' }
   validates :email, email: { message: 'is not a valid email address.' }
-  validates :github_link, presence: { message: 'cannot be blank.' }
+  validates :github_link, presence: { message: 'cannot be blank.' }, :id => :user_type == "coach"
   validates :github_link, uniqueness: { message: 'is associated with an existing account.' }
   validates :password, presence: { message: 'cannot be blank.' }, :if => :password
   validates :password, length: {minimum: 8, message: 'must be at least 8 characters.' }, :if => :password
