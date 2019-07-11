0343260c2421724840095ebc4affad30b51dcbc9
bbennett7 <bryn.bennett@gmail.com>
2019-07-11 16:07:27 -0700

    Fix find or create user through github login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,26 +19,22 @@ class SessionsController < ApplicationController
 
 #new session path when logging in through GitHub account
   def gh_create
-    console.log("here")
     @user = User.find_or_create_by(uid: auth['uid']) do |u|
-      console.log(u)
       u.username = auth['info']['nickname']
       u.first_name = auth['info']['name']
       u.email = auth['info']['email']
       u.profile_img = auth['info']['image']
       u.github_link = auth['info']['urls']['GitHub']
       u.uid = auth['uid']
-      u.type = "github"
+      u.user_type = "github"
       u.password = SecureRandom.urlsafe_base64(n=6)
-      console.log(u)
       u.save
-      console.log(u)
     end
 
     session[:user_id] = @user.id
 
     if @user.user_type == "github"
-      render choose_user_type_path
+      redirect_to choose_user_type_path
     else
       redirect_to user_path(@user)
     end
